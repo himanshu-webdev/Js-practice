29c1b264eec0be13589d5233af4b559d7a5aad92
Himanshu <hk9211420@gmail.com>
2024-12-09 20:53:10 +0530

    added

---
+++ 17_Weather/script.js
@@ -0,0 +1,4 @@
+const api = '2a367567648844e5444bc3c5e959f64b';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=delhi';
+
+//'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric'
