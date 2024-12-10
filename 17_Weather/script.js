ef3e84af3eda8b624290eb58e41b94c1b8352957
Himanshu <hk9211420@gmail.com>
2024-12-10 20:44:04 +0530

    added

--- 17_Weather/script.js
+++ 17_Weather/script.js
@@ -1,4 +1,52 @@
-const api = '2a367567648844e5444bc3c5e959f64b';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=delhi';
+const apiKey = '2a367567648844e5444bc3c5e959f64b';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-//'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric'
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  
+
+  if(response.status == 404){
+    document.querySelector(".error").style.display = "block"; 
+    document.querySelector(".weather").style.display = "none";   
+  } else{
+        const data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "images/clouds.png";   
+        } else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "images/clear.png";
+        } else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        } else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "images/mist.png";
+        } else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "images/rain.png";
+        } else if(data.weather[0].main == "Snow"){
+            weatherIcon.src = "images/snow.png";
+        } 
+
+        document.querySelector(".weather").style.display = "block";   
+        document.querySelector(".error").style.display = "none";  
+    }
+}
+
+  
+
+searchButton.addEventListener("click", function(){
+    checkWeather(searchBox.value);
+})
+
+searchBox.addEventListener("keydown", function(e){
+    if(e.key == "Enter"){
+        checkWeather(searchBox.value);
+    }
+})
